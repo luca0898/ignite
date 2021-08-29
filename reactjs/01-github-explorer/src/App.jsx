e292220b4e98ff69fa35171153bfe4083a4a49bc
luca0898 <lucaguiraldello@hotmail.com>
2021-08-29 17:32:40 -0300

    Estilização da listagem

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,12 +1,8 @@
-import { Counter } from './Components/Counter'
 import { RepositoryList } from './Components/RepositoryList'
 import './styles/global.scss'
 
 export function App() {
     return (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 }
