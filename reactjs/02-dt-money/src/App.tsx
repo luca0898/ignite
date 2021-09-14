b368001b6f8a2da50ddc415a0194976aa77081b9
luca0898 <lucaguiraldello@hotmail.com>
2021-09-13 22:48:24 -0300

    Configurando modal de criação

--- reactjs/02-dt-money/src/App.tsx
+++ reactjs/02-dt-money/src/App.tsx
@@ -1,32 +1,33 @@
+import { useState } from "react";
+import Modal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
-import { createServer } from "miragejs";
 import { GlobalStyle } from "./styles/global";
 
-createServer({
-  routes() {
-    this.namespace = "api";
-    this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      ]
-    })
-    
-  }
-})
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar Transação</h2>
+      </Modal>
       <GlobalStyle />
     </>
   );
