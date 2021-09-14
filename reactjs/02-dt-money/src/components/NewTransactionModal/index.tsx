7c667c3816a01b002b5b16ab1810acd7c83cfa58
luca0898 <lucaguiraldello@hotmail.com>
2021-09-13 23:05:08 -0300

    Estrutura do Formulário

--- reactjs/02-dt-money/src/components/NewTransactionModal/index.tsx
+++ reactjs/02-dt-money/src/components/NewTransactionModal/index.tsx
@@ -11,9 +11,28 @@ export function NewTransactionModal({ isOpen, onRequestClose: onRequestClone }: 
         <Modal
             isOpen={isOpen}
             onRequestClose={onRequestClone}
+            overlayClassName="react-modal-overlay"
+            className="react-modal-content"
         >
             <Container>
                 <h2>Cadastrar Transação</h2>
+
+                <input
+                    placeholder="Título"
+                />
+
+                <input
+                    type="number"
+                    placeholder="Valor"
+                />
+
+                <input
+                    placeholder="Categoria"
+                />
+
+                <button type="submit">
+                    Cadastrar
+                </button>
             </Container>
         </Modal>
     )
