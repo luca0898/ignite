82e09689da7f2045baa9a188eec8e940da1162b7
luca0898 <lucaguiraldello@hotmail.com>
2021-09-13 22:23:02 -0300

    Configurando MirageJS

--- reactjs/02-dt-money/src/components/TransactionTable/index.tsx
+++ reactjs/02-dt-money/src/components/TransactionTable/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export function TransactionTable(){
+export function TransactionTable() {
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/transactions')
+            .then(response => response.json())
+            .then(data => {
+                console.log({data})
+            })
+    }, [])
+
     return (
         <Container>
             <table>
