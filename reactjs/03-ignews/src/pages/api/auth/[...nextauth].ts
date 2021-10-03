37f847d29a95ef2febb66ce66c698b0beed059e0
luca0898 <lucaguiraldello@hotmail.com>
2021-10-03 18:03:16 -0300

    Salvando usuario no banco

--- reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
+++ reactjs/03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+import { fauna } from "../../../services/fauna"
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,23 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  jwt: {
+    signingKey: process.env.SIGNIN_KEY
+  },
+  callbacks: {
+    async signIn({ email }) {
+      console.log(email)
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'), { data: { email } }
+          )
+        )
+        return true
+      } catch (error) {
+        console.log(error)
+        return false;
+      }
+    },
+  }
 })
