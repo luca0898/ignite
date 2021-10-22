d643ddb7a6c6c705f2adade396b12db1a674639f
luca0898 <lucaguiraldello@hotmail.com>
2021-10-22 18:21:27 -0300

    Redirecionando para o Stripe

--- reactjs/03-ignews/src/Components/SubscribeButton/index.tsx
+++ reactjs/03-ignews/src/Components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -8,13 +10,27 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ productId }: SubscribeButtonProps) {
     const [session] = useSession();
 
-    function handleSubscribe(){
-        if (!session){
+    async function handleSubscribe() {
+        if (!session) {
             signIn('github')
             return;
         }
 
-        
+        try {
+            const response = await api.post<{ sessionId: any }>('/subscribe')
+
+            const { sessionId } = response.data
+
+            const stripe = await getStripeJs();
+
+            await stripe.redirectToCheckout({ sessionId });
+
+
+        } catch (error) {
+            alert(error.message)
+        }
+
+
     }
     return (
         <button
