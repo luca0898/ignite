676bce79f7ce8f507fb3419549ab4e4e6ec37eb0
luca0898 <lucaguiraldello@hotmail.com>
2022-01-30 19:21:30 -0300

    chapter ii - debugando a aplicação

--- nodejs/02-CarRentAPI/src/server.ts
+++ nodejs/02-CarRentAPI/src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
-import { createCourse } from "./routes";
-
 const app = express();
 
 app.use(express.json());
 
-app.get("/", createCourse);
+app.post("/courses", (request, response) => {
+    const { name } = request.body;
+
+    return response.json({ name });
+});
 
 app.listen(3333, () => console.log("🚀 Server is running!"));
