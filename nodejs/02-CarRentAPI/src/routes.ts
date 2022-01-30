762f6193ea3d6be459e532f70a54c4b9d79405c0
luca0898 <lucaguiraldello@hotmail.com>
2022-01-30 18:54:30 -0300

    chapter ii - typescript

---
+++ nodejs/02-CarRentAPI/src/routes.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express"
+import CreateCourseService from "./CreateCourseService"
+
+export function createCourse(request: Request, response: Response) {
+    CreateCourseService.execute({
+        name: "Nodejs",
+        duration: 10,
+        educator: "luca"
+    });
+
+    return response.json({ success: true })
+}
