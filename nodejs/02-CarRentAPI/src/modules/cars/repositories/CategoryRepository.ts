878d82eaadc08d32858c8d5f908537752bef8900
luca0898 <lucaguiraldello@hotmail.com>
2022-02-05 19:24:26 -0300

    conhecendo o singleton pattern

--- nodejs/02-CarRentAPI/src/modules/cars/repositories/CategoryRepository.ts
+++ nodejs/02-CarRentAPI/src/modules/cars/repositories/CategoryRepository.ts
@@ -4,8 +4,17 @@ import { ICategoryRepository, ICreateCategoryDTO } from "./ICategoryRepository";
 class CategoryRepository implements ICategoryRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoryRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoryRepository {
+        if (!CategoryRepository.INSTANCE) {
+            CategoryRepository.INSTANCE = new CategoryRepository();
+        }
+        return CategoryRepository.INSTANCE;
     }
 
     create({ name, description }: ICreateCategoryDTO): Category {
