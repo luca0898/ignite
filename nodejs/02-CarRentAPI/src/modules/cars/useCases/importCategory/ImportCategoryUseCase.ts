e9f683004bdcf84dc581d3f77d711f3218406d38
luca0898 <lucaguiraldello@hotmail.com>
2022-03-02 22:46:18 -0300

    Trabalhando com upload

---
+++ nodejs/02-CarRentAPI/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,57 @@
+import fs from "fs"
+import { parse as csvParse } from "csv-parse"
+import { ICategoryRepository } from "../../repositories/ICategoryRepository"
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
+
+class ImportCategoryUseCase {
+
+    constructor(private categoryRepository: ICategoryRepository) { }
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path)
+            const categories: IImportCategory[] = []
+            const parseFile = csvParse()
+
+            stream.pipe(parseFile)
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line
+
+                    categories.push({
+                        name,
+                        description
+                    })
+                })
+                .on("end", () => {
+                    resolve(categories)
+                })
+                .on("error", reject)
+
+        })
+    }
+
+    async execute(file: Express.Multer.File) {
+        const categories = await this.loadCategories(file);
+        console.log(categories)
+
+        categories.map(async ({ name, description }) => {
+            const existingCategory = this.categoryRepository.findByName(name)
+
+            if (!existingCategory) {
+                this.categoryRepository.create({
+                    name,
+                    description
+                })
+            }
+        })
+    }
+}
+
+export { ImportCategoryUseCase }
