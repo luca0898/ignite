6f0bde2cf0974faab72287d50c3690aa18128cb5
luca0898 <lucaguiraldello@hotmail.com>
2022-04-13 21:27:56 -0300

    docker files

--- nodejs/02-CarRentAPI/src/routes/categories.routes.ts
+++ nodejs/02-CarRentAPI/src/routes/categories.routes.ts
@@ -2,14 +2,14 @@ import { Router } from "express";
 import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
-import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 import { importCategoryController } from "../modules/cars/useCases/importCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
 
 const upload = multer({
-    dest: "./tmp"
-})
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -20,7 +20,7 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    return importCategoryController.handle(request, response)
+    return importCategoryController.handle(request, response);
 });
 
 export { categoriesRoutes };
